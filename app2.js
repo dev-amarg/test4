09ac39ae8fbc2541e9ee01e4f397367ea7b486da
amar <aayushi.amar12345@gmail.com>
2026-06-24 12:00:36 +0000

    first commit

---
+++ app2.js
@@ -0,0 +1,19 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 8000;
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello Docker Compose1 Node + MongoDB');
+});
+
+
+app.listen(port, () => {
+  console.log(`✅ Server running: http://localhost:${port}`);
+});
+
